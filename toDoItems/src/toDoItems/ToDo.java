082f53e2c3b54fc26dee95df9e7ddf011576bc86
Raghad-Tayeh <163656555+Raghad-Tayeh@users.noreply.github.com>
2024-10-02 19:26:03 +0300

    Add files via upload

--- toDoItems/src/toDoItems/ToDo.java
+++ toDoItems/src/toDoItems/ToDo.java
@@ -1,42 +1,16 @@
 package toDoItems;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ToDo {
-
-	String title;
-	String description;
-	int ID;
-	int counter = 1;
-	boolean flag = true;
+	
+	static String title;
+	static String description;
+	static int ID;
+	static int counter = 1;
+	static boolean flag = true;
 
 	Scanner input = new Scanner(System.in);
-	ArrayList<String[]> toDoItems = new ArrayList<>();
-
-	public void enterInfo() {
-
-		System.out.println("Please enter toDo item\s:-");
-
-		while (flag) {
-
-			System.out.print("Enter item title (enter (0) when done): ");
-			title = input.nextLine();
-
-			if (title.equals("0")) {
-
-				flag = false;
-				break;
-			}
-
-			System.out.print("Enter item description: ");
-			description = input.nextLine();
-			
-			toDoItems.add(new String[] {String.valueOf(counter), title, description});
-			
-			counter++;
-		}
-	}
 
 	public String exportType() {
 
